96ffc546645696e6a3267932cbcbad632e381b9a
Faraz Saeed <frazahmed94@gmail.com>
2026-07-09 13:01:20 +0500

    Add files via upload

---
+++ Loops.ts
@@ -0,0 +1,54 @@
+/*
+<<------------------------------------------------------------------------>> Looping Statements in TypeScript <<------------------------------------------------------------------------>>
+TypeScript supports three main loops:
+
+while loop
+do...while loop
+for loop
+
+// While Loop
+let i: number = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// Do...While Loop
+let j: number = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
+
+
+<<------------------------------------------------------------------------>> Example 1: Print Numbers 1 to 5
+
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+let j = -25;
+do {
+    console.log(j);
+    ++j;
+} while (j <= 5 && j < 0);
+
+
+let i: number = 1;
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+let i: number = 1;
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i);
+} 
+*/
+
